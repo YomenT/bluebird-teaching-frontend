d30e2a592a692d551c054a5ab9351060cb76dbfa
khansameer25 <realskhan25@gmail.com>
2023-07-27 14:34:27 -0400

    Updated contact form design

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 import "./css/ContactForm.css";
 import { showNotification } from "../firebase.js";
 
@@ -11,10 +11,13 @@ const ContactForm = () => {
     const formData = new FormData(formRef.current);
 
     try {
-      const response = await fetch('https://adminbluebirdteaching.pythonanywhere.com/send-email/', {
-        method: 'POST',
-        body: formData,
-      });
+      const response = await fetch(
+        "https://adminbluebirdteaching.pythonanywhere.com/send-email/",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
 
       const responseData = await response.json();
 
@@ -30,37 +33,83 @@ const ContactForm = () => {
   };
 
   return (
-    <form ref={formRef} onSubmit={sendEmail} className="form-container">
-      <div className="form-row">
-        <div className="form-item">
-          <label className="form-label" htmlFor="name">Name:</label>
-          <input className="form-input" type="text" id="name" name="name" required />
-        </div>
-        
-        <div className="form-item">
-          <label className="form-label" htmlFor="email">Email:</label>
-          <input className="form-input" type="email" id="email" name="email" required />
-        </div>
-      </div>
-
-      <div className="form-item">
-        <label className="form-label" htmlFor="attachments">Attachments:</label>
-        <input
-          className="form-input"
-          type="file"
-          id="attachments"
-          name="attachments"
-          multiple
+    <div className="contact-section">
+      <div className="contact-logo-container">
+        <img
+          style={{ marginBottom: "0px" }}
+          src={"BlueBird-Logo-Color-Light-Text.png"}
+          alt="Open"
+          className="contact-logo"
         />
       </div>
+      <form
+        ref={formRef}
+        onSubmit={sendEmail}
+        className="form-container p-body-xsmall"
+      >
+        <div className="form-row">
+          <div className="form-item">
+            <label className="form-label" htmlFor="name">
+              Name:
+            </label>
+            <input
+              className="form-input"
+              type="text"
+              id="name"
+              name="name"
+              required
+            />
+          </div>
 
-      <div className="form-item">
-        <label className="form-label" htmlFor="message">Message:</label>
-        <textarea className="form-input" id="message" name="message" rows="4" required />
-      </div>
+          <div className="form-item">
+            <label className="form-label" htmlFor="email">
+              Email:
+            </label>
+            <input
+              className="form-input"
+              type="email"
+              id="email"
+              name="email"
+              required
+            />
+          </div>
+        </div>
 
-      <button type="submit" className="button" style={{ width: "100%" }}>Send</button>
-    </form>
+        <div className="form-item">
+          <label className="form-label" htmlFor="attachments">
+            Attachments:
+          </label>
+          <input
+            className="form-input"
+            type="file"
+            id="attachments"
+            name="attachments"
+            multiple
+          />
+        </div>
+
+        <div className="form-item">
+          <label className="form-label" htmlFor="message">
+            Message:
+          </label>
+          <textarea
+            className="form-input"
+            id="message"
+            name="message"
+            rows="4"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="email-button button"
+          style={{ width: "100%" }}
+        >
+          Send
+        </button>
+      </form>
+    </div>
   );
 };
 
