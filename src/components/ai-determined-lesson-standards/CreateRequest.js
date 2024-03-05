a547324ede0eb745de9f29e91539327db153a69b
Shuhada' Al Watan <yomen@pop-os.localdomain>
2024-03-06 03:29:40 +0400

    Changing Components for AI Feature
    I was able to get the components within the collapsible to change
    on click, but I wasn't able to figure out how to get the slide
    animation to actually work. It still seems to just instantly change
    components on click.

---
+++ src/components/ai-determined-lesson-standards/CreateRequest.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CreateRequest() {
+
+    return (
+        <div>
+            <p className="p-body-small">Another Test</p>
+        </div>
+    )
+}
+
+export default CreateRequest
